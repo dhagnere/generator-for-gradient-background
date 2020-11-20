040e8c8828d79eb9251500c7ea230f21dc018eb6
wothag <33983572+wothag@users.noreply.github.com>
2020-11-20 11:32:20 +0100

    added RTL choice and Reload page functionnality

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
+var ltr = document.querySelector(".ltr")
+var btnReload = document.querySelector(".btn-reload");
 var body = document.getElementById("gradient");
 
+  
 function setGradient() {
   body.style.background =
     "linear-gradient(to right, "
@@ -10,8 +13,27 @@ function setGradient() {
     + ", "
     + color2.value
     + ")";
+  console.log(css);
+  css.textContent = body.style.background + ";";
+
+}
+function setGradient2() {
+  body.style.background =
+    "linear-gradient(to left, "
+    + color1.value
+    + ", "
+    + color2.value
+    + ")";
+  console.log(css);
+  css.textContent = body.style.background + ";";
+}
+
+function reloadPage() {
+  window.location.reload()
 }
 
 color1.addEventListener("input", setGradient);
-
 color2.addEventListener("input", setGradient);
+ltr.addEventListener("click", setGradient2);
+btnReload.addEventListener("click", reloadPage);
+
